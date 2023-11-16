73b6b1cdb716a01fdbca97e30dc80dfcd252aaca
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2023-11-16 23:05:07 +0530

    dom introduction

---
+++ dom/introduction/introduction.js
@@ -0,0 +1,18 @@
+
+// function printName() {
+//     console.log(window);
+// }
+
+/*
+    To do any changes with in the webpage dynamically , we need the access to that particular HTML element object.
+
+    document API / object present inside the window (globally) is the one knows everything about DOM tree.
+
+    doucment API has so many methods that will give us the desired HTML element Object.
+
+
+    1. Access to nodes ( html element object ) => Reading objects from DOM tree.
+    2. Manipulating a DOM tree node.(changing the behaviour of the nodes)
+    3. Deleting a node from the DOM tree.
+
+*/
