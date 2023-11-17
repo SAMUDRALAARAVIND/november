b2212e5ca5e5313245c3f84e6eb1b6d9753d3438
SAMUDRALAARAVIND <aravindsamudrala1708@gmail.com>
2023-11-17 20:06:46 +0530

    dom operations

--- dom/introduction/introduction.js
+++ dom/introduction/introduction.js
@@ -11,8 +11,19 @@
     doucment API has so many methods that will give us the desired HTML element Object.
 
 
-    1. Access to nodes ( html element object ) => Reading objects from DOM tree.
-    2. Manipulating a DOM tree node.(changing the behaviour of the nodes)
-    3. Deleting a node from the DOM tree.
+    1. Access to nodes ( html element object ) => Reading objects from DOM tree. => Read
+    2. Manipulating a DOM tree node.(changing the behaviour of the nodes) => Update
+    3. Deleting a node from the DOM tree. => Delete
+    4. Adding / creating a new node into the DOM tree => Create
+
+    CRUD
+
+
+    Events topic
+
+    Employee management 
+    Jira dashboard clone
+    Google sheet clone
+    Excalidraw
 
 */
